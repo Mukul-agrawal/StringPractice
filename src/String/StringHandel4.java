7d976c291ece5acfb1bc2466adae2031f4c71bb3
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:46:54 +0530

    add input and output

--- src/String/StringHandel4.java
+++ src/String/StringHandel4.java
@@ -5,5 +5,27 @@ import java.util.Scanner;
 public class StringHandel4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        char[] array = inputString.toCharArray();
+        int d = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                d = (d + 1) % 4;
+                array[i] = '*';
+                if (d == 1)
+                    array[i] = '*';
+                else if (d == 2)
+                    array[i] = '^';
+                else if (d == 3) {
+                    array[i] = '@';
+                    d = d + 1;
+                }
+            }
+        }
+        for (int i = 0; i < inputString.length(); i++) {
+            System.out.print(array[i]);
+        }
+        sc.close();
     }
 }
