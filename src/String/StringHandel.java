09a164ceea9994bf111ee49d955d1040d81f8859
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:16:20 +0530

    add input and output with final result

--- src/String/StringHandel.java
+++ src/String/StringHandel.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class StringHandel {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String string1 = sc.nextLine();
+        System.out.print("Index : ");
+        for (int i = 0; i < string1.length(); i++) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n" + "Chars : ");
+        for (int i = 0; i < string1.length(); i++) {
+            System.out.print(string1.charAt(i) + " ");
+        }
     }
 }
