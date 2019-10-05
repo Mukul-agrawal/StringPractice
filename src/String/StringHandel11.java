25689ab1eeffe96e780cf94f48638d7d9decd836
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 19:22:39 +0530

    add input and print output

--- src/String/StringHandel11.java
+++ src/String/StringHandel11.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class StringHandel11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        char ch = sc.nextLine().trim().charAt(0);
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == ch) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
     }
 }
