55dd4f592f1a413a2bf8e295059a3dbcbfb9dcdc
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:50:32 +0530

    add input and output

--- src/String/StringHandel5.java
+++ src/String/StringHandel5.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class StringHandel5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        String subString = sc.nextLine().trim();
+        String replace = sc.nextLine().trim();
+        inputString = inputString.replaceAll(subString, replace);
+        System.out.println(inputString);
+        sc.close();
     }
 }
