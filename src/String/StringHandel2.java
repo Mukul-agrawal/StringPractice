6092497a55256b659659c13ccb34c1dc821d742c
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:24:45 +0530

    Did changes in  class StringHandel2

--- src/String/StringHandel2.java
+++ src/String/StringHandel2.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class StringHandel2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Scanner scan = new Scanner(System.in);
-        String string = scan.nextLine();
-        int indexValue = Integer.parseInt(scan.nextLine());
+        String string = sc.nextLine();
+        int indexValue = Integer.parseInt(sc.nextLine());
         System.out.println(string.substring(indexValue));
     }
 }
--- src/String/StringHandel3.java
+++ src/String/StringHandel3.java
@@ -1,4 +1,9 @@
 package String;
 
+import java.util.Scanner;
+
 public class StringHandel3 {
+    public static void main(String[] args) {
+        Scanner sc =
+    }
 }
