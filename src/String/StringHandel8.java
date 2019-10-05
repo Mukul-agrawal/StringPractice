061cbb017d0bce3b38bc0cee0f8dd068dfba6581
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 19:06:02 +0530

    add input and print output

--- src/String/StringHandel8.java
+++ src/String/StringHandel8.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class StringHandel8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        int sum = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+            if (c > 64 && c < 92) {
+                sum = sum + inputString.charAt(i);
+            }
+        }
+        System.out.print(sum);
+        sc.close();
     }
 }
 
