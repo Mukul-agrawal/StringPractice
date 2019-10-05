d691e67eb0051b7481e5a9b97e90c9c857cd5a72
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:53:42 +0530

    add input and output

--- src/String/StringHandel6.java
+++ src/String/StringHandel6.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class StringHandel6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        System.out.println(inputString.toUpperCase());
     }
 }
