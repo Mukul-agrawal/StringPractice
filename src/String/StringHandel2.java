cd89989a9acf03887519b28a06cc613ac7eea9bf
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:20:30 +0530

    add input and output

--- src/String/StringHandel2.java
+++ src/String/StringHandel2.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class StringHandel2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+        int indexValue = Integer.parseInt(scan.nextLine());
+        System.out.println(string.substring(indexValue));
     }
 }
