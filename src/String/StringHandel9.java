812a442f4169851eeec7d90b1f5bde9b0fbcc36b
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 19:14:21 +0530

    add input and print output

--- src/String/StringHandel9.java
+++ src/String/StringHandel9.java
@@ -5,5 +5,20 @@ import java.util.Scanner;
 public class StringHandel9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        int upperWeight = 0;
+        int lowerWeight = 0;
+        int absoluteWeight;
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+            if (c > 64 && c < 91) {
+                upperWeight = upperWeight + inputString.charAt(i);
+            } else if (c > 96 && c < 124) {
+                lowerWeight = lowerWeight + inputString.charAt(i);
+            }
+        }
+        absoluteWeight = upperWeight - lowerWeight;
+        System.out.print("Absolute Weight is " + Math.abs(absoluteWeight));
+        sc.close();
     }
 }
