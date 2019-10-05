21ec416aae0e58c2197b83b9e55ff4ce93aef0e0
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2019-10-05 18:56:57 +0530

    add input and output

--- src/String/StringHandel7.java
+++ src/String/StringHandel7.java
@@ -5,5 +5,12 @@ import java.util.Scanner;
 public class StringHandel7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine().trim();
+        int sum = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            sum = sum + inputString.charAt(i);
+        }
+        System.out.println(sum);
+        sc.close();
     }
 }
