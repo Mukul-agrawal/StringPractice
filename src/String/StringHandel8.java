dec83cebfb43ea4389368f888b42e9d5c746e92f
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 12:32:47 +0530

    Did changes in StringHandel8

--- src/String/StringHandel8.java
+++ src/String/StringHandel8.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class StringHandel8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string :");
         String inputString = sc.nextLine().trim();
         int sum = 0;
         for (int i = 0; i < inputString.length(); i++) {
@@ -13,6 +14,7 @@ public class StringHandel8 {
                 sum = sum + inputString.charAt(i);
             }
         }
+        System.out.println("total weight of uppercase character :");
         System.out.print(sum);
         sc.close();
     }
