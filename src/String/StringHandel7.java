cd0da9b26a7e935cc3bbab3e6211557a2d5fb97b
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 12:29:17 +0530

    Did changes in StringHandel7.class

--- src/String/StringHandel7.java
+++ src/String/StringHandel7.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class StringHandel7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string :");
         String inputString = sc.nextLine().trim();
         int sum = 0;
         for (int i = 0; i < inputString.length(); i++) {
             sum = sum + inputString.charAt(i);
         }
+        System.out.println("total weight of given string :");
         System.out.println(sum);
         sc.close();
     }
