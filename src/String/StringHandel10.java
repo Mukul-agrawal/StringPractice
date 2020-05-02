901d52d228e94f8265f77d40b539fef6b0229b51
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 12:38:19 +0530

    Did changes in StringHandel10

--- src/String/StringHandel10.java
+++ src/String/StringHandel10.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class StringHandel10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string :");
         String inputString = sc.nextLine();
+        System.out.println("Enter a character :");
         char character = sc.nextLine().trim().charAt(0);
+        System.out.println("output :");
         System.out.println(inputString.indexOf(character));
         sc.close();
     }
