d0c4565f63d3e0560db5bd52926b0eecc151826c
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 12:03:00 +0530

    Did changes in StringHandel3

--- src/String/StringHandel3.java
+++ src/String/StringHandel3.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class StringHandel3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter some text : ");
         String string = sc.nextLine();
+        System.out.println("Replaced text with vowel : ");
         string = string.replaceAll("[AaEeIiOoUu]", "*");
         System.out.println(string);
     }
