792b99fb6ba99062319b395aec6f1349c437803d
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 12:23:21 +0530

    Did changes in StringHandel6

--- src/String/StringHandel6.java
+++ src/String/StringHandel6.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class StringHandel6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string :");
         String inputString = sc.nextLine().trim();
+        System.out.println("String in uppercase :");
         System.out.println(inputString.toUpperCase());
     }
 }
