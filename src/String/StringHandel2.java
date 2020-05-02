911ffaaa07477e9b704b4cbc5cf847b280112322
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-02 12:00:39 +0530

    Did changes in StringHandel2

--- src/String/StringHandel2.java
+++ src/String/StringHandel2.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class StringHandel2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter some text : ");
         String string = sc.nextLine();
+        System.out.println("Enter the index: ");
         int indexValue = Integer.parseInt(sc.nextLine());
         System.out.println(string.substring(indexValue));
     }
